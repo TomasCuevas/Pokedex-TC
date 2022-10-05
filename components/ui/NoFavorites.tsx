44c7e800f2d91ea8c2d5fa650fda66c1cd8a82fa
Tomas Cuevas <memo_mister398@hotmail.com>
2022-10-05 09:54:52 -0300

    fix no favorite text align

--- components/ui/NoFavorites.tsx
+++ components/ui/NoFavorites.tsx
@@ -2,11 +2,13 @@ import { Container, Text, Image } from "@nextui-org/react";
 
 export const NoFavorites: React.FC = () => {
   return (
-    <Container className="flex flex-col h-[calc(100vh_-_140px)] items-center justify-center">
-      <Text h1>No hay favoritos</Text>
+    <Container className="flex h-[calc(100vh_-_140px)] flex-col items-center justify-center">
+      <Text className="text-center" h1>
+        No hay favoritos
+      </Text>
       <Image
         src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/132.svg"
-        className="w-[250px] h-[250px] opacity-5"
+        className="h-[250px] w-[250px] opacity-5"
         alt="no favorite"
       />
     </Container>
