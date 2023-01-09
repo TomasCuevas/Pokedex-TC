fc37eb9c36b75578481481fc62fb913271102bb9
Tomas Cuevas <memo_mister398@hotmail.com>
2023-01-09 17:17:07 -0300

    style: :dizzy: add animations on PokemonCard

--- components/pokemon/PokemonCard.tsx
+++ components/pokemon/PokemonCard.tsx
@@ -1,4 +1,32 @@
 import { useRouter } from "next/router";
+import { motion, Variants } from "framer-motion";
+
+//* animation variants *//
+const articleVariants: Variants = {
+  offscreen: { opacity: 0.8 },
+  onscreen: { opacity: 1, transition: { duration: 0.5 } },
+};
+
+const imageVariants: Variants = {
+  offscreen: { opacity: 0, scale: 0 },
+  onscreen: {
+    opacity: 1,
+    scale: [0, 1.1, 1],
+    transition: { duration: 0.8 },
+  },
+  hover: {
+    opacity: 1,
+    scale: 1.5,
+    rotate: 30,
+    y: -20,
+    transition: { duration: 0.4 },
+  },
+};
+
+const nameVariants: Variants = {
+  offscreen: { opacity: 0, x: 50 },
+  onscreen: { opacity: 1, x: 0, transition: { duration: 0.5 } },
+};
 
 //* interfaces *//
 import { IPokemon } from "../../interfaces/pokemonList";
@@ -11,23 +39,42 @@ export const PokemonCard: React.FC<IPokemon> = ({ id, img, name }) => {
   };
 
   return (
-    <article
+    <motion.article
+      initial="offscreen"
+      whileInView="onscreen"
+      variants={articleVariants}
+      viewport={{ once: true, amount: 0.1 }}
       onClick={onNavigate}
       className="group flex w-[calc(50%_-_16px)] scale-100 flex-col transition-all duration-300 md:w-[calc(33%_-_16px)] lg:w-[calc(25%_-_16px)] xl:w-[calc(20%_-_16px)] 2xl:w-[calc(16%_-_16px)]"
     >
       <div className="flex w-full cursor-pointer  flex-col items-center rounded-2xl bg-slate-900  transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl hover:shadow-white/20">
         <div className="w-[200px]">
-          <img src={img} alt={name} className="h-[200px] w-full" />
+          <motion.img
+            initial="offscreen"
+            whileInView="onscreen"
+            whileHover="hover"
+            viewport={{ once: true, amount: 0.1 }}
+            variants={imageVariants}
+            src={img}
+            alt={name}
+            className="h-[200px] w-full"
+          />
         </div>
         <footer className="flex w-full justify-between px-3 py-2">
-          <h3 className="text-xl font-semibold capitalize text-slate-300 group-hover:text-slate-100">
+          <motion.h3
+            initial="offscreen"
+            whileInView="onscreen"
+            variants={nameVariants}
+            viewport={{ once: true, amount: 0.1 }}
+            className="text-xl font-semibold capitalize text-slate-300 group-hover:text-slate-100"
+          >
             {name}
-          </h3>
+          </motion.h3>
           <span className="text-xl font-semibold capitalize text-slate-300 group-hover:text-slate-100">
             #{id}
           </span>
         </footer>
       </div>
-    </article>
+    </motion.article>
   );
 };
--- pages/pokemon/types/[type].tsx
+++ pages/pokemon/types/[type].tsx
@@ -1,4 +1,5 @@
 import { NextPage, GetStaticPaths, GetStaticProps } from "next";
+import { motion, Variants } from "framer-motion";
 
 //* layout *//
 import { Layout } from "../../../components/layouts";
@@ -9,6 +10,18 @@ import { PokemonCard } from "../../../components/pokemon";
 //* services *//
 import { getPokemonsForTypeService } from "../../../services";
 
+//* animation variants *//
+const imageAnimation: Variants = {
+  offscreen: { opacity: 0, x: -50 },
+  onscreen: { opacity: 1, x: 0, transition: { delay: 0.3, duration: 0.5 } },
+};
+
+const titleAnimation: Variants = {
+  offscreen: { opacity: 0, x: 50 },
+  onscreen: { opacity: 1, x: 0, transition: { delay: 0.3, duration: 0.5 } },
+};
+
+//* interface *//
 interface Props {
   allPokemons: [{ id: string; name: string; img: string }];
   type: string;
@@ -19,14 +32,24 @@ const PokemonTypesPage: NextPage<Props> = ({ allPokemons, type }) => {
     <Layout title={`${type.toLocaleUpperCase()} | Pokedex TC`}>
       <section className="mb-5 w-full px-[5%]">
         <div className="flex items-center gap-2">
-          <img
+          <motion.img
+            initial="offscreen"
+            whileInView="onscreen"
+            viewport={{ once: true, amount: 0.1 }}
+            variants={imageAnimation}
             src={`/img/types/${type}.png`}
             alt={type}
             className="h-[80px] w-[80px]"
           />
-          <h1 className="text-5xl font-bold capitalize tracking-tighter text-slate-100">
+          <motion.h1
+            initial="offscreen"
+            whileInView="onscreen"
+            variants={titleAnimation}
+            viewport={{ once: true, amount: 0.1 }}
+            className="text-5xl font-bold capitalize tracking-tighter text-slate-100"
+          >
             {type}
-          </h1>
+          </motion.h1>
         </div>
       </section>
       <section className="flex flex-wrap justify-center gap-4">
